103e0996cead26933e83db0262bc18eb39f50a9b
JonasHell <jonas.hellgoth@gmx.de>
2025-07-15 13:59:57 +0200

    avoid padding in z in my case

--- MAESTER/dataset.py
+++ MAESTER/dataset.py
@@ -169,6 +169,23 @@ class BetaSegDataset2D(torch.utils.data.dataset.Dataset):
 
 @register_plugin("dataset", "MyBetaSegDataset2D")
 class MyBetaSegDataset2D(BetaSegDataset2D):
+    def __init__(self, cfg):
+        self.path_list = cfg["path_list"]
+        self.pad = cfg["pad"]
+        self.aug = get_plugin("transform", cfg["aug"])(cfg)
+        self.vol_size = (
+            cfg["vol_size"] + int(cfg["vol_size"] // cfg["patch_size"])
+            if cfg["patch_size"] % 2 == 0
+            else cfg["vol_size"]
+        )
+        self.key_name_dict = {
+            x: x.split("/")[-1].split("_tensor")[0] for x in self.path_list
+        }
+        self.data_list = [
+            F.pad(torch.load(x)[self.key_name_dict[x]], [self.pad for _ in range(4)])
+            for x in self.path_list
+        ]
+
     def sample_cord(self, data_idx, axis):
         data = self.data_list[data_idx]  # get dataset
         _, d_z, d_x, d_y = data.shape
